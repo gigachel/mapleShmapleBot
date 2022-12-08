d2aafee8197c5d31e6c1dac3517be9e766736d47
stas <gigachel@gmail.com>
2022-12-08 20:22:47 +0300

    ~

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ dotenv.config();
 const port = 3000;
 const bot = new Telegraf(process.env.TOKEN);
 const app = Fastify();
-const webhookDomain = "maple-shmaple-bot.vercel.app";
+const webhookDomain = "persian-blue-rabbit-belt.cyclic.app";
 
-const webhook = await bot.createWebhook({ domain: webhookDomain, port: port });
+const webhook = await bot.createWebhook({ domain: webhookDomain });
 
 console.log("[LOG] : bot.secretPathComponent()", bot.secretPathComponent());
 app.post('/'+bot.secretPathComponent(), (req, rep) => webhook(req.raw, rep.raw));
 bot.on("text", ctx => ctx.reply("Hello"));
 
 app.get("/", async (request, reply) => {
-    return "!";
+    return "!!";
 });
 
 app.listen({ port: port }).then(() => console.log("Listening on port", port));
