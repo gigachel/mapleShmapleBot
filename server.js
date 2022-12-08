b05ae860905e5477f4d71638dd4f1c14e865a33f
stas <gigachel@gmail.com>
2022-12-08 20:34:56 +0300

    ~

--- server.js
+++ server.js
@@ -9,14 +9,34 @@ const bot = new Telegraf(process.env.TOKEN);
 const app = Fastify();
 const webhookDomain = "persian-blue-rabbit-belt.cyclic.app";
 
-const webhook = await bot.createWebhook({ domain: webhookDomain });
-
-console.log("[LOG] : bot.secretPathComponent()", bot.secretPathComponent());
-app.post('/'+bot.secretPathComponent(), (req, rep) => webhook(req.raw, rep.raw));
 bot.on("text", ctx => ctx.reply("Hello"));
 
-app.get("/", async (request, reply) => {
-    return "!!";
-});
+// const webhook = await bot.createWebhook({ domain: webhookDomain });
 
-app.listen({ port: port }).then(() => console.log("Listening on port", port));
+// console.log("[LOG] : bot.secretPathComponent()", bot.secretPathComponent());
+// app.post('/'+bot.secretPathComponent(), (req, rep) => webhook(req.raw, rep.raw));
+
+// app.get("/", async (request, reply) => {
+//     return "!!";
+// });
+
+// app.listen({ port: port }).then(() => console.log("Listening on port", port));
+
+
+bot.launch({
+    webhook: {
+      // Public domain for webhook; e.g.: example.com
+      domain: webhookDomain,
+  
+      // Port to listen on; e.g.: 8080
+    //   port: port,
+  
+      // Optional path to listen for.
+      // `bot.secretPathComponent()` will be used by default
+    //   hookPath: webhookPath,
+  
+      // Optional secret to be sent back in a header for security.
+      // e.g.: `crypto.randomBytes(64).toString("hex")`
+    //   secretToken: randomAlphaNumericString,
+    },
+  });
