0b0c53b4b6debcec62d76c89a208d4bc19e90c8e
stas <gigachel@gmail.com>
2022-12-22 13:39:56 +0300

    ~

--- server/index.js
+++ server/index.js
@@ -124,13 +124,13 @@ server.setErrorHandler(function (error, request, reply) {
     }
 })();
 
-// async function closeGracefully(signal) {
-//    console.log(`*^!@4=> Received signal to terminate: ${signal}`)
+async function closeGracefully(signal) {
+    console.log(`*^!@4=> Received signal to terminate: ${signal}`);
 
-//    await fastify.close()
-//    // await db.close() if we have a db connection in this app
-//    // await other things we should cleanup nicely
-//    process.kill(process.pid, signal);
-// }
-// process.once('SIGINT', closeGracefully)
-// process.once('SIGTERM', closeGracefully)
+    //    await fastify.close()
+    //    // await db.close() if we have a db connection in this app
+    //    // await other things we should cleanup nicely
+    //    process.kill(process.pid, signal);
+}
+process.once("SIGINT", closeGracefully);
+process.once("SIGTERM", closeGracefully);
