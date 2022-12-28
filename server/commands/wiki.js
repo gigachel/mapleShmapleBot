d53cfd5371506b3e8f119bacfdf20a07b48fa569
stas <gigachel@gmail.com>
2022-12-28 19:43:49 +0300

    ~

--- server/commands/wiki.js
+++ server/commands/wiki.js
@@ -1,5 +1,10 @@
+import { InlineKeyboard } from "grammy";
+
 export async function wikiConvers(conversation, ctx) {
+    let message = "";
+    let messageOptions = {};
     let search = ctx.match;
+    const searchLimit = 5;
 
     if (!search || !search.trim()) {
         await ctx.reply("Напиши слово, которое надо найти");
@@ -15,7 +20,7 @@ export async function wikiConvers(conversation, ctx) {
             "https://ru.wikipedia.org/w/api.php?" +
                 new URLSearchParams({
                     action: "opensearch",
-                    limit: 5,
+                    limit: searchLimit,
                     namespace: 0,
                     redirects: "resolve",
                     format: "json",
@@ -25,28 +30,43 @@ export async function wikiConvers(conversation, ctx) {
         )
     ).json();
 
-    const firstMatch = articlesList[1][0];
-    // firstMatch = firstMatch.replace(" ", "_");
+    if (articlesList[1] && articlesList[1].length) {
+        const firstMatch = articlesList[1].shift();
+        // firstMatch = firstMatch.replace(" ", "_");
 
-    const article = await (
-        await fetch(
-            "https://ru.wikipedia.org/w/api.php?" +
-                new URLSearchParams({
-                    action: "query",
-                    prop: "extracts",
-                    exintro: "",
-                    explaintext: "",
-                    format: "json",
-                    utf8: "",
-                    titles: firstMatch,
-                })
-        )
-    ).json();
+        const article = await (
+            await fetch(
+                "https://ru.wikipedia.org/w/api.php?" +
+                    new URLSearchParams({
+                        action: "query",
+                        prop: "extracts",
+                        exintro: "",
+                        explaintext: "",
+                        format: "json",
+                        utf8: "",
+                        titles: firstMatch,
+                    })
+            )
+        ).json();
 
-    const page = Object.values(article.query.pages)[0];
+        const page = Object.values(article.query.pages)[0];
 
-    // return ctx.reply(page.extract || "😵");
-    return ctx.reply(page.extract + JSON.stringify(articlesList, null, 2) || "😵");
+        message = page.extract || "😵";
+
+        if (articlesList[1].length) {
+            let inlineKeyboard = new InlineKeyboard();
+            articlesList[1].forEach((article) => {
+                inlineKeyboard = inlineKeyboard.text(article, article + "111").row();
+            });
+
+            messageOptions.reply_markup = inlineKeyboard;
+        }
+    } else {
+        message = "😵";
+    }
+
+    return ctx.reply(message, messageOptions);
+    // return ctx.reply(page.extract + JSON.stringify(articlesList, null, 2) || "😵");
 
     // Markup.keyboard(["one", "two", "three", "four", "five", "six"], {
     //     columns: parseInt(ctx.match[1]),
