405521c5b8e1c21e46972f80541b1ae092bd419b
stas <gigachel@gmail.com>
2024-08-19 09:26:04 +0300

    prisma migration

---
+++ server/prisma/migrations/20240819061253_new_fields/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - Added the required column `tg_id` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tg_id" TEXT NOT NULL,
+    "name" TEXT,
+    "first_name" TEXT,
+    "last_name" TEXT
+);
+INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
